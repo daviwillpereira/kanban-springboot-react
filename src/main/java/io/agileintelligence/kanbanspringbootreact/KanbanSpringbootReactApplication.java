2587a3233e97e0be10dbf38d1dc58d32e3e5c70d
daviwillpereira <daviwillpereira@gmail.com>
2022-02-22 17:47:18 -0300

    project first commit

---
+++ src/main/java/io/agileintelligence/kanbanspringbootreact/KanbanSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.kanbanspringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanSpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanSpringbootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agileintelligence/kanbanspringbootreact/KanbanSpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.kanbanspringbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanSpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
